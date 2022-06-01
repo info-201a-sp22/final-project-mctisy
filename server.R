53fcde41d771aee21cd603b6669c107200f53756
Vincent Chen <vjc6600@uw.edu>
2022-05-31 19:49:05 -0700

    vincent changes

--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(plotly)
 library(dplyr)
+library(bslib)
 
 covid_df <- read.csv("worldometer_coronavirus_summary_data.csv")
 covid_daily_df <- read.csv("worldometer_coronavirus_daily_data.csv")
@@ -8,10 +9,25 @@ covid_daily_df <- read.csv("worldometer_coronavirus_daily_data.csv")
 server <- function(input, output) {
 
   output$covid_plot <- renderPlotly({
-
-    plot <- NULL
-
-    return(plot)
+    filtered_df <- covid_df %>%
+      group_by(continent) %>%
+      summarize(total_confirmed = mean(total_confirmed, na.rm = TRUE),
+                total_deaths = mean(total_deaths, na.rm = TRUE), 
+                total_recovered = mean(total_recovered, na.rm = TRUE),
+                active_cases = mean(active_cases, na.rm = TRUE),
+                serious_or_critical = mean(serious_or_critical, na.rm = TRUE))
+   
+       
+covid_plot <-  ggplot(data = filtered_df) +
+      geom_bar(mapping = aes(x= continent, y = .data[[input$user_selection]]), stat = "identity") +
+       labs(title = "COVID data", 
+           x = "COntinents", 
+           y = "number of people",
+           color = "Legend Title")
+    
+    
+    
+    return(covid_plot)
   })
-
 }
+
--- ui.R
+++ ui.R
@@ -1,7 +1,10 @@
 library(plotly)
+library(bslib)
 
 covid_df <- read.csv("worldometer_coronavirus_summary_data.csv")
 covid_daily_df <- read.csv("worldometer_coronavirus_daily_data.csv")
+
+features <- c("total_confirmed", "total_deaths", "total_recovered", "active_cases", "serious_or_critical")
 
 intro_tab <- tabPanel(
   "Introduction",
@@ -13,23 +16,33 @@ intro_tab <- tabPanel(
 
 
 sidebar_panel_widget <- sidebarPanel(
-  # selectInput(
-  #   inputId = "user_selection",
-  #   label = "YOUR CODE HERE",
-  #   choices = climate_df$country,
-  #   multiple = F, 
-  # )
-)
+  selectInput(
+    inputId = "user_selection",
+    label = "Country Options",
+    choices = features,
+    selected = "United States"
+  ))
+
+
 
 main_panel_plot <- mainPanel(
-  plotlyOutput(outputId = "covid_plot")
+  plotlyOutput(outputId = "covid_plot"),
+  p("this plot is good because yes")
 )
 
-climate_tab <- tabPanel(
-  "Climate Viz",
+Page_1 <- tabPanel(
+  "covid stats by continent",
   sidebarLayout(
     sidebar_panel_widget,
     main_panel_plot
+  )
+)
+
+summary_tab <- tabPanel(
+  "Summary Page",
+  fluidPage(
+    h1("Summary"),
+    p("summary info")
   )
 )
 
@@ -37,5 +50,7 @@ climate_tab <- tabPanel(
 ui <- navbarPage(
   "Climate Change",
   intro_tab,
-  climate_tab
+  page_1,
+  summary_tab
 )
+
