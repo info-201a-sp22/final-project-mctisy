91a3f0ae2b49b895dfcd9cb8a60fc6b5938624de
mctisy <hhuuucc@hotmail.com>
2022-06-01 18:00:24 -0700

    update format

--- server.R
+++ server.R
@@ -7,26 +7,27 @@ covid_df <- read.csv("worldometer_coronavirus_summary_data.csv")
 covid_daily_df <- read.csv("worldometer_coronavirus_daily_data.csv")
 
 server <- function(input, output) {
-
   output$covid_plot <- renderPlotly({
     filtered_df <- covid_df %>%
       group_by(continent) %>%
-      summarize(total_confirmed = mean(total_confirmed, na.rm = TRUE),
-                total_deaths = mean(total_deaths, na.rm = TRUE), 
-                total_recovered = mean(total_recovered, na.rm = TRUE),
-                active_cases = mean(active_cases, na.rm = TRUE),
-                serious_or_critical = mean(serious_or_critical, na.rm = TRUE))
-   
-       
-covid_plot <-  ggplot(data = filtered_df) +
-      geom_bar(mapping = aes(x= continent, y = .data[[input$user_selection]] / 100000), stat = "identity") +
-       labs(title = "COVID Statistics by Continent", 
-           x = "Continents", 
-           y = "Number of People in Millions",
-           color = "Legend Title") + theme_minimal()
-    
-    
+      summarize(
+        total_confirmed = mean(total_confirmed, na.rm = TRUE),
+        total_deaths = mean(total_deaths, na.rm = TRUE),
+        total_recovered = mean(total_recovered, na.rm = TRUE),
+        active_cases = mean(active_cases, na.rm = TRUE),
+        serious_or_critical = mean(serious_or_critical, na.rm = TRUE)
+      )
+
+    covid_plot <- ggplot(data = filtered_df) +
+      geom_bar(mapping = aes(x = continent, y = .data[[input$user_selection]] / 100000), stat = "identity") +
+      labs(
+        title = "COVID Statistics by Continent",
+        x = "Continents",
+        y = "Number of People in Millions",
+        color = "Legend Title"
+      ) +
+      theme_minimal()
+
     return(covid_plot)
   })
 }
-
--- ui.R
+++ ui.R
@@ -8,31 +8,36 @@ covid_daily_df <- read.csv("worldometer_coronavirus_daily_data.csv")
 
 features <- c("total confirmed", "total deaths", "total recovered", "active cases", "serious or critical")
 
-my_theme <- bs_theme(bg = "#faf7f8", 
-                     fg = "#4a3a40",
-                     primary = "#87495f") 
+my_theme <- bs_theme(
+  bg = "#faf7f8",
+  fg = "#4a3a40",
+  primary = "#87495f"
+)
 
 sidebar_panel_widget <- sidebarPanel(
   selectInput(
     inputId = "user_selection",
     label = "COVID Stat Option",
-    choices = c("Total Confirmed" = "total_confirmed",
-                "Total Deaths" = "total_deaths", 
-                "Total Recovered" = "total_recovered",
-                "Active Cases" = "active_cases",
-                "Serious or Critical" = "serious_or_critical"),
+    choices = c(
+      "Total Confirmed" = "total_confirmed",
+      "Total Deaths" = "total_deaths",
+      "Total Recovered" = "total_recovered",
+      "Active Cases" = "active_cases",
+      "Serious or Critical" = "serious_or_critical"
+    ),
     selected = "United States"
-  ))
+  )
+)
 
 main_panel_plot <- mainPanel(
   plotlyOutput(outputId = "covid_plot"),
-  p("This plot attempts to show how each continent is affected by COVID-19 
-    differently. It shows different stats like deaths, recoveries, new cases, 
-    critical conditions and total cases and compares it with the other 
+  p("This plot attempts to show how each continent is affected by COVID-19
+    differently. It shows different stats like deaths, recoveries, new cases,
+    critical conditions and total cases and compares it with the other
     continents.")
 )
 
-############ Tabpanels 
+############ Tabpanels
 intro_tab <- tabPanel(
   "Introduction",
   fluidPage(
@@ -45,7 +50,7 @@ page_1 <- tabPanel(
   sidebarLayout(
     sidebar_panel_widget,
     main_panel_plot,
-  ), 
+  ),
 )
 
 summary_tab <- tabPanel(
@@ -53,10 +58,10 @@ summary_tab <- tabPanel(
   fluidPage(
     h1("Summary"),
     h3("Summary Takeaway 1"),
-    p("As you can see from the Covid stats by continent, Africa and 
+    p("As you can see from the Covid stats by continent, Africa and
     Australia/Oceania were affected by COVID-19 the least. They had the least
     deaths, confirmed cases, critical cases, and active cashes. On the other
-    hand, South America and Europe were affected the most by having the most 
+    hand, South America and Europe were affected the most by having the most
     deaths, active cases, confirmed cases, and critical cases. With each feature
     you can predict insights that could potentially explain what was going on
     with each country. For example, the high active cases within Europe could
@@ -72,6 +77,7 @@ summary_tab <- tabPanel(
 ui <- navbarPage(
   "Worldwide COVID-19 Statistics",
   intro_tab,
-  page_1, theme = my_theme,
-  summary_tab)
-
+  page_1,
+  theme = my_theme,
+  summary_tab
+)
